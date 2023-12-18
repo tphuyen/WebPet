9c8b2ae560ba1ae8b43021b62239a60b08f10311
thangvb <bathangvu@gmail.com>
2023-12-19 00:25:53 +0700

    FIX: icon, dropdown, z-index

--- animation.js
+++ animation.js
@@ -1,22 +1,22 @@
-$(document).ready(function() {
-    // Xử lý khi di chuột vào tag "Chuyên mục thú cưng"
-    $('.pet').mouseover(function() {
-        // Hiển thị dropdown-menu khi di chuột vào
-        $('.dropdown-menu').css('display', 'block');
-    });
+// $(document).ready(function() {
+//     // Xử lý khi di chuột vào tag "Chuyên mục thú cưng"
+//     $('.pet').mouseover(function() {
+//         // Hiển thị dropdown-menu khi di chuột vào
+//         $('.dropdown-menu').css('display', 'block');
+//     });
 
-    // Xử lý khi di chuột ra khỏi dropdown-menu
-    $('.dropdown-menu').mouseleave(function() {
-        // Ẩn dropdown-menu khi di chuột ra khỏi dropdown-menu
-        $(this).css('display', 'none');
-    });
+//     // Xử lý khi di chuột ra khỏi dropdown-menu
+//     $('.dropdown-menu').mouseleave(function() {
+//         // Ẩn dropdown-menu khi di chuột ra khỏi dropdown-menu
+//         $(this).css('display', 'none');
+//     });
 
-    // Xử lý khi click vào dropdown-item
-    $('.dropdown-item').click(function() {
-        // Lấy nội dung của dropdown-item
-        var selectedItem = $(this).text();
-        console.log('Selected Item:', selectedItem);
-        // Ẩn dropdown-menu sau khi chọn
-        $('.dropdown-menu').css('display', 'none');
-    });
-});
+//     // Xử lý khi click vào dropdown-item
+//     $('.dropdown-item').click(function() {
+//         // Lấy nội dung của dropdown-item
+//         var selectedItem = $(this).text();
+//         console.log('Selected Item:', selectedItem);
+//         // Ẩn dropdown-menu sau khi chọn
+//         $('.dropdown-menu').css('display', 'none');
+//     });
+// });
